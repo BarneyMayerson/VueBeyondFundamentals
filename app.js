f6263c117e98deba3dc28051bf79c7b1b93117c0
Barney Mayerson <mr.barney.mayerson@gmail.com>
2023-06-29 10:42:26 +0300

    04-003 Using Lifecycle Hooks

--- app.js
+++ app.js
@@ -3,7 +3,39 @@ const vm = Vue.createApp({
     return {
       message: "Hello world!"
     }
-  }
+  },
+
+  beforeCreate() {
+    console.log("beforeCreate ", this.message);
+  },
+
+  created() {
+    console.log("created ", this.message);
+  },
+
+  beforeMount() {
+    console.log("beforeMount ", this.$el);
+  },
+
+  mounted() {
+    console.log("mounted ", this.$el);
+  },
+
+  beforeUpdate() {
+    console.log("beforeUpdate ");
+  },
+
+  updated() {
+    console.log("updated ", this.$el);
+  },
+
+  beforeUnmount() {
+    console.log("beforeUnmount ");
+  },
+
+  unmounted() {
+    console.log("unmounted ");
+  },
 });
 
 vm.mount("#app");
